d40cba478fc8b89adfb00529980836e8b1acf031
Fernando Fernandez Mancera <ffmancera@riseup.net>
2023-10-27 14:01:13 +0200

    HSR: add support to HSR/PRP interface
    This patch add support to HSR/PRP interface. Please notice that PRP
    driver is represented as HSR too. They are different drivers but on
    kernel they are integrated together.
    
    The PRP property decides whether to use HSR or PRP protocol.

--- src/core/devices/nm-device.c
+++ src/core/devices/nm-device.c
@@ -5448,6 +5448,7 @@ nm_device_get_route_metric_default(NMDeviceType device_type)
     case NM_DEVICE_TYPE_WIMAX:
         return 250;
     case NM_DEVICE_TYPE_BOND:
+    case NM_DEVICE_TYPE_HSR:
         return 300;
     case NM_DEVICE_TYPE_TEAM:
         return 350;
@@ -18284,7 +18285,7 @@ set_property(GObject *object, guint prop_id, const GValue *value, GParamSpec *ps
         nm_assert(priv->type == NM_DEVICE_TYPE_UNKNOWN);
         priv->type = g_value_get_uint(value);
         nm_assert(priv->type > NM_DEVICE_TYPE_UNKNOWN);
-        nm_assert(priv->type <= NM_DEVICE_TYPE_LOOPBACK);
+        nm_assert(priv->type <= NM_DEVICE_TYPE_HSR);
         break;
     case PROP_LINK_TYPE:
         /* construct-only */
