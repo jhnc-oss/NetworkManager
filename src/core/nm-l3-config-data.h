e727fa625f358665e3734abf5ede1ee044b1f6f2
Mary Strodl <ipadlover8322@gmail.com>
2025-02-04 22:40:17 -0500

    clat: propagate network_id down to l3cfg

--- src/core/nm-l3-config-data.h
+++ src/core/nm-l3-config-data.h
@@ -486,6 +486,10 @@ NMUtilsIPv6IfaceId nm_l3_config_data_get_ip6_token(const NML3ConfigData *self);
 
 gboolean nm_l3_config_data_set_ip6_token(NML3ConfigData *self, NMUtilsIPv6IfaceId ipv6_token);
 
+gboolean nm_l3_config_data_set_network_id(NML3ConfigData *self, const char *network_id);
+
+const char *nm_l3_config_data_get_network_id(const NML3ConfigData *self);
+
 gboolean nm_l3_config_data_set_pref64_valid(NML3ConfigData *self, gboolean val);
 
 gboolean nm_l3_config_data_get_pref64_valid(const NML3ConfigData *self, gboolean *out_val);
