600c29c4bde783d2eff0bdd634ef1f0027eec1a0
Mr Sambucca <github@h4ck.info>
2025-04-10 20:47:12 +0000

    Edit nm-setting-ip-config.c

--- src/libnm-core-impl/nm-setting-ip-config.c
+++ src/libnm-core-impl/nm-setting-ip-config.c
@@ -642,7 +642,9 @@ nm_ip_route_new(int         family,
         .family   = family,
         .dest     = canonicalize_ip_binary(family, &dest_bin, FALSE),
         .prefix   = prefix,
-        .next_hop = canonicalize_ip_binary(family, (next_hop && *next_hop != '\0') ? &next_hop_bin : NULL, TRUE),
+        .next_hop = canonicalize_ip_binary(family,
+                                            (next_hop && *next_hop != '\0') ? &next_hop_bin : NULL,
+                                            TRUE),
         .metric   = metric,
     };
 
